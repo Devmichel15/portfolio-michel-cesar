510fd90047e1ba557fd20c2f730627c00cecca3b
devmichel15 <httpmichel15cesar@gmail.com>
2025-12-15 13:11:40 +0100

    ajustando a importação das imgs

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ export const projects = [
   {
     id: 1,
     name: "Cadastro de Usuários",
-    image: "../public/project1.png",
+    image: "/project1.png",
     description:
       "Projeto de cadastro de usuários utilizando React e armazenando os dados no Local Storage do navegador.",
     link: "https://github.com/Devmichel15/cadastro-de-users",
@@ -20,7 +20,7 @@ export const projects = [
   {
     id: 2,
     name: "SaaS de gestão financeira",
-    image: "../public/project3.png",
+    image: "/project3.png",
     description:
     "Sistema de gestão financeira para pequenas empresas, com funcionalidades de controle de despesas, receitas e geração de relatórios.",
     link: "https://github.com/Devmichel15/gestao-ativa-app",
@@ -30,7 +30,7 @@ export const projects = [
   {
     id: 3,
     name: "Loja de Acessórios Online",
-    image: "../public/project2.png",
+    image: "/project2.png",
     description:
     "Um website de e-commerce para venda de acessórios de moda, com carrinho de compras e integração com API do Whatsapp.",
     link: "https://github.com/Devmichel15/Acessory-Store",
@@ -40,7 +40,7 @@ export const projects = [
   {
       id: 4,
       name: "Landing Page para o Restaurante | Café Del Mar",
-    image: "../public/project4.png",
+    image: "/project4.png",
     description:
       "Landing page para o restaurante Café Del Mar, com informações sobre cardápio e localização.",
     link: "https://github.com/Devmichel15/site-del-mar",
@@ -50,7 +50,7 @@ export const projects = [
   {
       id: 5,
     name: "Landing Page para o Influencer | Nuno Baio",
-    image: "../public/project6.png",
+    image: "/project6.png",
     description:
       "Landing page para o influencer Nuno Baio, com informações sobre seus serviços e contato.",
     link: "https://github.com/Devmichel15/website-nuno-baio",
@@ -60,7 +60,7 @@ export const projects = [
   {
     id: 6,
     name: "Landing Page para a Influencer | Jessi Madalena",
-    image: "../public/project5.png",
+    image: "/project5.png",
     description:
       "Landing page para a influencer Jessi Madalena, com informações sobre seus serviços e contato.",
       link: "https://github.com/Devmichel15/site-jessi",
